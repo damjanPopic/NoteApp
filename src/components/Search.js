11d26f3bea3ef78f2369aa865ad35ef7dbf32e36
Damjan Popic <damjanpopic@MacBook-Air-von-Damjan.local>
2022-02-24 14:07:54 +0100

    create search input without logic

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <div className="search-container">
+      <input placeholder="Search for your notes"></input>
+    </div>
+  );
+};
+export default Search;
